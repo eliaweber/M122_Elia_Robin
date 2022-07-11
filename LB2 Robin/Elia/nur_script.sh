4e998fd008e0f73f8c57c3fcfb67a00421a5d86c
eliaweber <elia.weber@edu.tbz.ch>
2022-07-11 21:19:08 +0200

    Elia Teil hinzugefügt

---
+++ LB2 Robin/Elia/nur_script.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+curl wttr.in/rapperswil?n1 |  grep "°C" | head -n 1 >wetter.txt
+
+grep -o "[+-][0-9][0-9]" wetter.txt >wetter2.txt
+
+Zahl=$(grep -o "[0-9][0-9]" wetter2.txt)
+
+echo $Zahl
+
+rm rapperswil.png
+
+wget wttr.in/rapperswil.png
+
+
+if [ $Zahl -gt 30 ]; then
+
+mpack -s "Wetterdienst" -d "30.txt" "rapperswil.png" eliaweber57@gmail.com
+
+
+elif [ $Zahl -gt 20 ]; then
+
+mpack -s "Wetterdienst" -d "20.txt" "rapperswil.png" eliaweber57@gmail.com
+
+elif [ $Zahl -gt 10 ]; then
+
+mpack -s "Wetterdienst" -d "10.txt" "rapperswil.png" eliaweber57@gmail.com
+
+elif [ $Zahl -gt 0 ]; then
+
+mpack -s "Wetterdienst" -d "0.txt" "rapperswil.png" eliaweber57@gmail.com
+
+fi
